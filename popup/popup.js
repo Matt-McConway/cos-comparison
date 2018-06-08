7b21db6620ea2f63ef8e895f59bfecacaef525dd
Matt McConway <mmcc800@aucklanduni.ac.nz>
2018-06-08 18:01:19 +1200

    Basic extension flow
    Built in a hacky way - will refine it later. Currently gets relevant tabs and puts them into a form

--- popup/popup.js
+++ popup/popup.js
@@ -1,9 +1,35 @@
-document.onload = getTabs();
+document.onload = main();
 
-function getTabs(){
-    chrome.tabs.query({}, function(tabs){
-        alert(tabs[0].url);
-    });
+function main(){
+   //Gets all cosDNA Product tabs in the current window
+   chrome.tabs.query({"url":"http://www.cosdna.com/eng/cosmetic_*"}, function(tabs){
+       if (tabs[0]){
+            var products= [];
+            var checkboxes = "";
+            for (i=0; i < tabs.length; i++){
+                var ingredientsLoc = tabs[i].title.indexOf(" ingredients");
+                products.push(tabs[i].title.slice(0, ingredientsLoc));
+            }
+            for (i=0; i < products.length; i++){
+                checkboxes += '<div class="form-check">' +
+                              '<input class="form-check-input" type="checkbox" value="" id="defaultCheck' + i +'">' +
+                              '<label class="form-check-label" for="defaultCheck'+ i + '">' +
+                              products[i] + '<hr></label> </div>'
+            }
+            console.log(products);
+            document.getElementById("tabsResponse").innerHTML =
+                `<h6 id="popupH6">We've detected these products:</h6>
+                <form id="popupForm">
+                    <div class="form-group" id="productChecklist"></div>
+                </form>
 
-
+                <button type="button" class="btn btn-primary" id="popupCompareButton">Compare!</button>`;
+            document.getElementById("productChecklist").innerHTML = checkboxes;
+            document.getElementById("popupCompareButton").onclick = function(){chrome.tabs.create({"url":"../comparison/comparison.html"}, function(tab){
+            })};
+       }else{
+        document.getElementById("tabsResponse").innerHTML = `<h6 id="popupH6">We did not detect any products.</h6> 
+                                                             <p>Visit <a href="http://www.cosdna.com/" target="_blank">cosdna.com</a> to find some.</p>`;
+       }
+   });
 };
