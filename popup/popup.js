3ba6c4f78f3080692bdd19eec815307f2c1584b0
Matt McConway <mmcc800@aucklanduni.ac.nz>
2018-06-19 00:45:58 +1200

    Readability Changes
    Also changed improved product form style with consistant left alignment and product title length limit.

--- popup/popup.js
+++ popup/popup.js
@@ -1,35 +1,54 @@
 document.onload = main();
 
+
 function main(){
    //Gets all cosDNA Product tabs in the current window
-   chrome.tabs.query({"url":"http://www.cosdna.com/eng/cosmetic_*"}, function(tabs){
+   const baseURL = {"url": "http://www.cosdna.com/eng/cosmetic_*"}
+   chrome.tabs.query(baseURL, function(tabs){
        if (tabs[0]){
             var products= [];
-            var checkboxes = "";
+            var checkboxHTML = "";
             for (i=0; i < tabs.length; i++){
-                var ingredientsLoc = tabs[i].title.indexOf(" ingredients");
-                products.push(tabs[i].title.slice(0, ingredientsLoc));
+                // Isolates the product title by removing " ingredients | CosDNA"
+                var ingredientsIndex = tabs[i].title.indexOf(" ingredients");
+                products.push(tabs[i].title.slice(0, ingredientsIndex));
             }
             for (i=0; i < products.length; i++){
-                checkboxes += '<div class="form-check">' +
-                              '<input class="form-check-input" type="checkbox" value="" id="defaultCheck' + i +'">' +
-                              '<label class="form-check-label" for="defaultCheck'+ i + '">' +
-                              products[i] + '<hr></label> </div>'
+                // Adds each product to the checkbox form
+
+                var productTitleShortened = ""; // First limit the length of the title
+                if (products[i].length > 20){
+                    productTitleShortened = products[i].slice(0, 20) + "...";
+                } else{
+                    productTitleShortened = products[i];
+                }
+                checkboxHTML += `
+                                <div class="form-check checkboxDiv">
+                                    <input class="form-check-input" type="checkbox" value="${products[i]}" id="product${i}">
+                                    <label class="form-check-label" for="product${i}">${productTitleShortened}<hr></label>
+                                </div>
+                                `
             }
-            console.log(products);
+
             document.getElementById("tabsResponse").innerHTML =
-                `<h6 id="popupH6">We've detected these products:</h6>
+                `
+                <h6 id="popupH6">We've detected these products:</h6>
                 <form id="popupForm">
                     <div class="form-group" id="productChecklist"></div>
                 </form>
 
-                <button type="button" class="btn btn-primary" id="popupCompareButton">Compare!</button>`;
-            document.getElementById("productChecklist").innerHTML = checkboxes;
+                <button type="button" class="btn btn-primary" id="popupCompareButton">Compare!</button>
+                `;
+            document.getElementById("productChecklist").innerHTML = checkboxHTML; // Populate form with products
+
             document.getElementById("popupCompareButton").onclick = function(){chrome.tabs.create({"url":"../comparison/comparison.html"}, function(tab){
+                // Create the comparison page
             })};
        }else{
-        document.getElementById("tabsResponse").innerHTML = `<h6 id="popupH6">We did not detect any products.</h6> 
-                                                             <p>Visit <a href="http://www.cosdna.com/" target="_blank">cosdna.com</a> to find some.</p>`;
+        document.getElementById("tabsResponse").innerHTML = `
+                                                             <h6 id="popupH6">We did not detect any products.</h6>
+                                                             <p>Visit <a href="http://www.cosdna.com/" target="_blank">cosdna.com</a> to find some.</p>
+                                                            `;
        }
    });
 };
